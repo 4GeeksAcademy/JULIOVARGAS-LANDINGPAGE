4c0d7a4d5bfc58da7507807ab0907e475af9d0ec
JulioV10 <117667674+JulioV10@users.noreply.github.com>
2023-06-15 20:24:50 +0000

    LandingPageProject

---
+++ src/js/component/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+import Navbar from "./navbar.jsx"
+import Jumbotron from "./jumbotron.jsx";
+import Footer from "./footer.jsx"
+import Cards from "./cards.jsx";
+
+//create your first component
+const Home = () => {
+	return (
+		<div className="text-center">
+			<Navbar />
+			<Jumbotron />
+			<Cards />
+			<Footer />
+		</div>
+	);
+};
+
+export default Home;
