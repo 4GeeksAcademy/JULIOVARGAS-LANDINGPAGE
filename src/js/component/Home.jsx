4096c94245c490472a2160151613e32b34b5c421
JulioV10 <117667674+JulioV10@users.noreply.github.com>
2023-06-15 21:07:28 +0000

    cambios

--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-import Navbar from "./navbar.jsx"
-import Jumbotron from "./jumbotron.jsx";
-import Footer from "./footer.jsx"
-import Cards from "./cards.jsx";
+import Navbar from "./Navbar.jsx"
+import Jumbotron from "./Jumbotron.jsx";
+import Footer from "./Footer.jsx"
+import Cards from "./Cards.jsx";
 
 //create your first component
 const Home = () => {
